5ba75a1c20f1bfffe2ab3c515640077368231dc9
Latifa-ka <85689676+Latifa-ka@users.noreply.github.com>
2023-04-11 23:16:43 +0300

    Add files via upload

---
+++ src./index.js
@@ -0,0 +1,8 @@
+import React from"react";
+// import BodyParser from"body-paser";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Header from "./components/header";
+// import Ejs from "ejs";
+
+ReactDOM.render(<div><App /></div>,document.getElementById("root"));
